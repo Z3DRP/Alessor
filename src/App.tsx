77722607fc6b189a7e0559791322be39d56db9bb
Zach Palmer <apex1421@outlook.com>
2024-02-29 14:10:16 -0600

    loader, toast, button components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { toast, ToastContainer } from 'react-toastify';
+import Loader from './components/Loader/Loader';
+import ToastOptions from './types/ToastOptions';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const showToast = (message: string, options: ToastOptions): Promise<void> => {
+    return new Promise<void>((resolve, reject) => {
+      try {
+        if (options.type === 'success') {
+          console.log(`success: ${message}`);
+          toast.success(message);
+        } else if (options.type === 'error') {
+          console.log(`error ${message}`);
+          toast.error(message);
+
+        } else if (options.type === 'warning') {
+          console.log(`warning: ${message}`);
+          toast.warning(message);
+        } else if (options.type === 'info') {
+          console.log(`info: ${message}`);
+          toast.info(message);
+        }
+        resolve();
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="alessor">
+        <ToastContainer />
+        <Loader isLoading={isLoading} />
+      </div>
+    </>
+  )
 }
 
 export default App;
